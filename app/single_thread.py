b1d0d7d15cc80b418acf53d9f0aed5b75f322546
osawa-koki <koki@osawa.dev>
2024-01-02 20:51:29 +0900

    `poetry run python -m black ./app/ ./tests/`

--- app/single_thread.py
+++ app/single_thread.py
@@ -12,13 +12,13 @@ def single_thread():
     zs = np.empty((len(xs), len(ys)))
     for i, x in enumerate(xs):
         for j, y in enumerate(ys):
-            z = x + y*1j
+            z = x + y * 1j
             for n in range(max_iter):
                 if abs(z) > 2:
                     zs[i, j] = n
                     break
-                z = z*z + x + y*1j
+                z = z * z + x + y * 1j
             else:
                 zs[i, j] = max_iter
-    plt.imshow(zs.T, cmap='bone', extent=(x_min, x_max, y_min, y_max))
-    plt.savefig('mandelbrot-single_thread.png')
+    plt.imshow(zs.T, cmap="bone", extent=(x_min, x_max, y_min, y_max))
+    plt.savefig("mandelbrot-single_thread.png")
