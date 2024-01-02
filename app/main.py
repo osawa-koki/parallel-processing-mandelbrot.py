320ba2cca4afd85d5b0a3f790f0dd0d374c40323
osawa-koki <koki@osawa.dev>
2024-01-02 21:52:59 +0900

    マルチスレッド処理の実装。

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 import timeit
 
 from single_thread import single_thread
+from multi_thread import multi_thread
 
 number = 1
 
 print(f"single_thread: {timeit.timeit(single_thread, number=number)}")
+print(f"multi_thread: {timeit.timeit(multi_thread, number=number)}")
