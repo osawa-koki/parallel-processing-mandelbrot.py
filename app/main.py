0f31c4737adeac289e719d33b69edb18107b9a7d
osawa-koki <koki@osawa.dev>
2024-01-07 19:09:03 +0900

    small fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
-import timeit
+# import timeit
 
 from single_thread import single_thread
 # from multi_thread import multi_thread
 
-number = 1
+# number = 1
 
 # print(f"single_thread: {timeit.timeit(single_thread, number=number)}")
 # print(f"multi_thread: {timeit.timeit(multi_thread, number=number)}")
